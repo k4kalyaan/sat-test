80463f29f6e490e3086572b414d8c28202e14080
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2022-05-08 14:10:58 +0530

    updated

--- DIY-TF-final.tf
+++ DIY-TF-final.tf
@@ -64,7 +64,7 @@ resource "ibm_is_security_group_rule" "ssh_outbound" {
 }
 
 resource "ibm_is_image" "custom_image" {
-  name             = "${var.vpc_name}-Ubuntu-os-20.04"
+  name             = "${var.vpc_name}-Ubuntu-os-20"
   href             = var.image_url
   operating_system = "ubuntu-20-amd64"
   resource_group   = data.ibm_resource_group.rg.id
